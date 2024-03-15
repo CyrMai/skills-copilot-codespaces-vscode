bca6a000b6c16c620c3e3f3bd2e4463f571c2e6a
CyrMai <157713638+CyrMai@users.noreply.github.com>
2024-03-15 09:58:32 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,70 @@
+// Create web server
+// Create a web server that listens on port 3000. When you visit http://localhost:3000/ in your web browser, the server should respond with "Hello, World!"
+
+var http = require('http');
+var port = 3000;
+
+var requestHandler = function(request, response) {
+  console.log('New request to: ' + request.url);
+  response.end('Hello, World!');
+};
+
+var server = http.createServer(requestHandler);
+
+server.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
+// Path: comments.js
+// Create a web server
+// Create a web server that listens on port 3000. When you visit http://localhost:3000/ in your web browser, the server should respond with "Hello, World!"
+
+var http = require('http');
+var port = 3000;
+
+var requestHandler = function(request, response) {
+  console.log('New request to: ' + request.url);
+  response.end('Hello, World!');
+};
+
+var server = http.createServer(requestHandler);
+
+server.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
+// Path: comments.js
+// Create a web server
+// Create a web server that listens on port 3000. When you visit http://localhost:3000/ in your web browser, the server should respond with "Hello, World!"
+
+var http = require('http');
+var port = 3000;
+
+var requestHandler = function(request, response) {
+  console.log('New request to: ' + request.url);
+  response.end('Hello, World!');
+};
+
+var server = http.createServer(requestHandler);
+
+server.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
+// Path: comments.js
+// Create a web server
+// Create a web server that listens on port 3000. When you visit http://localhost:3000/ in your web browser, the server should respond with "Hello, World!"
+
+var http = require('http');
+var port = 3000;
+
+var requestHandler = function(request, response) {
+  console.log('New request to: ' + request.url);
+  response.end('Hello, World!');
+};
+
+var server = http.createServer(requestHandler);
+
+server.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
+// Path: comments.js
+// Create a web server
+// Create a web server that listens
